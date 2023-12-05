deae1d210f995909ac765ee9c9d77f05017461dd
Santellana179 <imjuan179@gmail.com>
2023-12-04 19:53:06 -0600

    Agregar archivos de proyecto.

---
+++ grafoAct/Grafo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafoAct
+{
+    internal class Grafo
+    {
+        private int V; // Número de vértices
+        private List<int>[] adyacencia; // Lista de adyacencia
+
+        // Constructor
+        public Grafo(int v)
+        {
+            V = v;
+            adyacencia = new List<int>[v];
+            for (int i = 0; i < v; ++i)
+            {
+                adyacencia[i] = new List<int>();
+            }
+        }
+
+        // Método para agregar una arista al grafo no dirigido
+        public void AgregarArista(int v, int w)
+        {
+            adyacencia[v].Add(w);
+            adyacencia[w].Add(v);
+        }
+
+        // Método para imprimir el grafo
+        public void ImprimirGrafo()
+        {
+            for (int i = 0; i < V; ++i)
+            {
+                Console.Write("Vértice " + i + ": ");
+                foreach (var adyacente in adyacencia[i])
+                {
+                    Console.Write(adyacente + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ grafoAct/Program.cs
@@ -0,0 +1,16 @@
+﻿using grafoAct;
+// Crear un grafo con 5 vértices
+Grafo grafo = new Grafo(5);
+
+// Agregar aristas
+grafo.AgregarArista(0, 1);
+grafo.AgregarArista(0, 4);
+grafo.AgregarArista(1, 2);
+grafo.AgregarArista(1, 3);
+grafo.AgregarArista(1, 4);
+grafo.AgregarArista(2, 3);
+grafo.AgregarArista(3, 4);
+
+// Imprimir el grafo
+Console.WriteLine("Grafo no dirigido:");
+grafo.ImprimirGrafo();
